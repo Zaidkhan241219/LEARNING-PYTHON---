6a09ea67a821b5424ee9106e7f7497069cc264c8
ZAID KHAN <zk14062006@gmail.com>
2026-07-25 00:58:27 +0530

    Updated oddoreven.py

---
+++ oddoreven.py
@@ -0,0 +1,8 @@
+print("This Code Will Tell You Which One Is Odd And Which One Is Even.......")
+
+user_choice = int(input("Enter Any Number = "))
+if (user_choice % 2 ==0 ):
+    print("Number Is Even......")
+else : 
+    print("Number Is Odd......")
+    
