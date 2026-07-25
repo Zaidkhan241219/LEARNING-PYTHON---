67f1a68af42a518785635cb3b0464f9e7b7c2461
ZAID KHAN <zk14062006@gmail.com>
2026-07-25 15:44:52 +0530

    Day 2 - Loops

---
+++ Python_Chp/Chp_04/Numbers.py
@@ -0,0 +1,19 @@
+#All about INT FLOAT COMPLEX
+
+#INT 
+age=20
+marks=50
+temperature1=-5
+
+print("age = " , age  , "marks = " , marks, "temperature = ",temperature1)
+
+#FLOAT
+price=25.55
+temperature2=-2.5
+percentage=2.5
+print("price = " , price , "temperature = " , temperature2, "percentage = ",percentage)
+
+#COMPLEX 
+number= 3 + 4j
+print(number.real)
+print(number.imag)
