90c509410c0cda6d5598e578f76e44e01f6e9df4
ZAID KHAN <zk14062006@gmail.com>
2026-07-25 15:15:38 +0530

    Day 2 - Loops

---
+++ Basic_Imp_Things/Print_with_seperator.py
@@ -0,0 +1,9 @@
+#This code will teach use case of SEPERATOR and END parameter also 
+
+#SEPERATOR
+print("Python" , "C++" , "Java" , sep=" | ")
+
+#END PARAMETER
+print("Python", end= " - ")
+print("C++" , end= " - ")
+print("Java")
