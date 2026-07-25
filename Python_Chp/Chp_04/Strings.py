083e1741d50c7e4c79b4775c8724658b04232216
ZAID KHAN <zk14062006@gmail.com>
2026-07-25 16:17:24 +0530

    Day 2 - Loops

--- Python_Chp/Chp_04/Strings.py
+++ Python_Chp/Chp_04/Strings.py
@@ -1,1 +1,17 @@
 #All about STRINGS 
+
+#SIGLE QUOTE 
+print('SIGLE QUOTE :- ')
+print('Hello I am Single Quote String')
+
+
+#DOUBLE QUOTE 
+print("DOUBLE QUOTE :- ")
+print("Hello I am Double Quote String")
+
+
+#TRIPLE QUOTE 
+print('''TRIPLE QUOTE :- ''')
+print('''Hello I am Triple Quote
+you can print in next line also using me ''')
+
