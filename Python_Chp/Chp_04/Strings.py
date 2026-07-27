392b63ec21b0a3dc5a9a56d0e4b606111a8e1098
ZAID KHAN <zk14062006@gmail.com>
2026-07-27 14:39:13 +0530

    Day 2 - Loops

--- Python_Chp/Chp_04/Strings.py
+++ Python_Chp/Chp_04/Strings.py
@@ -1,17 +1,36 @@
 #All about STRINGS 
 
+
 #SIGLE QUOTE 
-print('SIGLE QUOTE :- ')
-print('Hello I am Single Quote String')
+# print('SIGLE QUOTE :-')
+# name1='Aman'
+# print(name1)
 
 
 #DOUBLE QUOTE 
-print("DOUBLE QUOTE :- ")
-print("Hello I am Double Quote String")
+# print('DOUBLE QUOTE :-')
+# name2="Hello I am double quote string"
+# print(name2)
 
 
 #TRIPLE QUOTE 
-print('''TRIPLE QUOTE :- ''')
-print('''Hello I am Triple Quote
-you can print in next line also using me ''')
+# print('TRIPLE QUOTE :-')
+# name3="""Python is a powerful language.
+# Understanding python is very much easy.
+# Python is begginer friendly."""
+# print(name3)
+
+
+# STRING INDEXING 
+# name="Python Is Not A Snake"
+
+#+VE INDEXING
+# print(name[2])
+# print(name[3:11])
+# print(name[7:18:5])
+
+#-VE INDEXING
+# print(name[-2])
+# print(name[3:-10])
+# print(name[::-1])  #REVERSING A STRING
 
