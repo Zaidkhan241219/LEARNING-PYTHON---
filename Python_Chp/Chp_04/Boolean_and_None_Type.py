54fe040e755c7c09521f8d5c6575b988afa51b63
ZAID KHAN <zk14062006@gmail.com>
2026-07-27 14:49:53 +0530

    Day 2 - Loops

---
+++ Python_Chp/Chp_04/Boolean_and_None_Type.py
@@ -0,0 +1,14 @@
+# File Name :- Boolean_and_None_Type.py
+
+#BOOLEAN (TRUE or FALSE)
+
+is_active = True
+is_availabe = False
+print(is_active)
+print(is_availabe)
+print(type(is_availabe))
+
+#NONE (EMPTY VALUE not ZERO)
+
+result = None
+print(result)
