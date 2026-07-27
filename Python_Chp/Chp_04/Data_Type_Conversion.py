c447d918fad11242f68dae0b4b72433503b18c34
ZAID KHAN <zk14062006@gmail.com>
2026-07-27 15:13:41 +0530

    Day 2 - Loops

---
+++ Python_Chp/Chp_04/Data_Type_Conversion.py
@@ -0,0 +1,51 @@
+#File Name : Data_Type_Conversion.py
+
+# CONVERTING TO INT
+
+# str ----------> int
+# age ="100"
+# print("Before Conversion Type = " , type(age))
+# converted_to_int = int(age)
+# print(converted_to_int)
+# print("After Conversion Type = " , type(converted_to_int))
+
+# float ----------> int
+# marks=13.7
+# print("Before Conversion Type = " , type(marks))
+# converted_to_int = int(marks)
+# print(converted_to_int)
+# print("After Conversion Type = " , type(converted_to_int))
+
+
+# CONVERTING TO FLOAT
+
+# str ----------> float
+# age ="100"
+# print("Before Conversion Type = " , type(age))
+# converted_to_float = float(age)
+# print(converted_to_float)
+# print("After Conversion Type = " , type(converted_to_float))
+
+# # int ----------> float
+# marks=13
+# print("Before Conversion Type = " , type(marks))
+# converted_to_float = float(marks)
+# print(converted_to_float)
+# print("After Conversion Type = " , type(converted_to_float))
+
+
+# CONVERTING TO FLOAT
+
+# int ----------> str
+age =100
+print("Before Conversion Type = " , type(age))
+converted_to_str = str(age)
+print(converted_to_str)
+print("After Conversion Type = " , type(converted_to_str))
+
+# float ----------> str
+marks=13.7
+print("Before Conversion Type = " , type(marks))
+converted_to_str = str(marks)
+print(converted_to_str)
+print("After Conversion Type = " , type(converted_to_str))
