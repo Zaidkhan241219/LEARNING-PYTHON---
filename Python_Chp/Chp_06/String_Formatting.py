a64bd261357de670ac6d1196bbf1b4050bdbb822
ZAID KHAN <zk14062006@gmail.com>
2026-08-01 14:00:13 +0530

    string formatting done

---
+++ Python_Chp/Chp_06/String_Formatting.py
@@ -0,0 +1,18 @@
+# File Name :- String_Formatting.py
+
+# USE OF 'f-strings'
+# num1=int(input("Enter First Number : ").strip())
+# num2=int(input("Enter First Number : ").strip())
+# print(f"SUM = {num1+num2} \nDIFFERENCE = {num1-num2} \nMULTIPLICATION = {num1*num2} \nDIVISION = {num1/num2} \nMODULUS = {num1%num2}")
+
+
+# USE OF 'format()'
+# num1=int(input("Enter First Number : ").strip())
+# num2=int(input("Enter First Number : ").strip())
+# print("SUM = {} \nDIFFERENCE = {} \nMULTIPLICATION = {} \nDIVISION = {} \nMODULUS = {}".format(num1+num2,num1-num2,num1*num2,num1/num2,num1%num2))
+
+
+# USE OF 'format()'
+# num1=int(input("Enter First Number : ").strip())
+# num2=int(input("Enter First Number : ").strip())
+# print("SUM = %s \nDIFFERENCE = %d \nMULTIPLICATION = %s \nDIVISION = %d \nMODULUS = %s"%(num1+num2,num1-num2,num1*num2,num1/num2,num1%num2))
