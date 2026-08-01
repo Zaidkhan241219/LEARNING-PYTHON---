599dc5e4e5464d8506e14d85b7eaa497b6878569
ZAID KHAN <zk14062006@gmail.com>
2026-08-01 14:03:43 +0530

    string indexing done

---
+++ Python_Chp/Chp_06/String_Indexing.py
@@ -0,0 +1,13 @@
+# File Name :- String_Indexing.py
+
+# name="Python Is Not A Snake"
+
+#+VE INDEXING
+# print(name[2])
+# print(name[3:11])
+# print(name[7:18:5])
+
+#-VE INDEXING
+# print(name[-2])
+# print(name[3:-10])
+# print(name[::-1])  #REVERSING A STRING
