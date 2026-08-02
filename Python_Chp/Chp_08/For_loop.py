a00db878e7303f5edc15d61732a24f000de56fd5
ZAID KHAN <zk14062006@gmail.com>
2026-08-02 14:20:35 +0530

    for loop done

--- Python_Chp/Chp_08/For_loop.py
+++ Python_Chp/Chp_08/For_loop.py
@@ -29,27 +29,88 @@ Find the factorial of a given number.
 Example Input: 5 Output: 120"""
 
 # num = int(input("Enter The Number: "))
-
-# fact = 1
-
+# # fact = 1
 # if num == 0 or num == 1:
 #     print(f"Factorial of {num} is = 1")
 # else:
 #     for i in range(1, num + 1):
 #         fact *= i
+# #     print(f"Factorial of {num} is = {fact}")
 
-#     print(f"Factorial of {num} is = {fact}")
-    
 """Question 5 (For Loop)
 Print the following pattern:
 *
 **
 ***
 ****
-*****
-""" 
+***** """ 
 
-for i in range(1,6):
-    for j in range(1,6):
-        if i==j:
-           print("*"*i)
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if i==j:
+#            print("*"*i)
+
+"""Question 6 (For Loop)
+Print the Fibonacci series for the first N terms.
+Example
+Input: 7
+Output:  0  1  1  2  3  5  8 """
+
+# print("ENTER THE NUMBER TILL THE FIBONACCI SERIES YOU WANT")
+# num=int(input("Enter The Number Of Terms : ").strip())
+# a , b = 0 , 1
+# for i in range(num) :
+#     print(a , end="  ")
+#     c=a+b
+#     a=b
+#     b=c
+
+"""Question 7 (For Loop)
+Check whether a given number is a Prime Number."""
+
+# num=int(input("Enter The Number : ").strip())
+
+# if num<=1:
+#     print("Invalid Number")
+# else:
+#     for i in range(2,num):
+#         if num%i==0:
+#             print("Number Is Not Prime")
+#             break
+#         else:
+#             print("Number Is prime")
+
+"""Question 8 (For)
+Create a Number Guessing Game.
+Rules:
+Secret number = 27
+Keep asking the user to guess until they get it right.
+Print "Too High" or "Too Low" after each wrong guess.
+Count the total number of attempts.
+After the correct guess, print a star pattern where the number of stars equals the number of attempts.
+Example
+Guess: 15
+Too Low
+Guess: 40
+Too High
+Guess: 27
+Correct!
+Attempts: 3"""
+
+# secret_number=39
+# attempt=0
+# for i in range(1,100):
+#     guess=int(input("Guess Your Number : "))
+#     attempt+=1
+#     if guess>secret_number:
+#         print("Too High")
+#     elif secret_number>guess:
+#         print("Too Low")
+#     else:
+#         print("You Guessed It Correct !!")
+#         print(f"You Made {attempt} Attempts")
+#         print("Star Pattern:")
+#         for j in range(1,attempt+1):
+#             print("*"*j)
+#         break
+
--- Python_Chp/Chp_08/While_loop.py
+++ Python_Chp/Chp_08/While_loop.py
@@ -43,19 +43,90 @@ Check whether a number is a Palindrome."""
 #        print("The Number You Typed Is Pallidrome")
 #else :
 #        print("Typed Number Is Not A Pallidrome")    
-        
+
 # Int Ke Saath 
-num = int(input("Enter The Number: "))
+# num = int(input("Enter The Number: "))
+# original = num
+# reverse = 0
+# while num > 0:
+#     digit = num % 10
+#     reverse = reverse * 10 + digit
+#     num = num // 10
+# if original == reverse:
+#     print("The Number You Typed Is Palindrome")
+# else:
+#     print("The Number You Typed Is Not A Palindrome")
 
-original = num
-reverse = 0
+"""Question 5 (While Loop)
+Find the sum of digits of a given number.
+Example Input: 5829 Output: 24"""
 
-while num > 0:
-    digit = num % 10
-    reverse = reverse * 10 + digit
-    num = num // 10
+# num = int(input("Enter The Number: "))
+# sum = 0
+# while num > 0:
+#     digit = num % 10
+#     sum += digit
+#     num = num // 10
+# print("Sum of digits =", sum)
 
-if original == reverse:
-    print("The Number You Typed Is Palindrome")
-else:
-    print("The Number You Typed Is Not A Palindrome")
+"""Question 6 (While Loop)
+Keep asking the user to enter a password until the correct password (python123) is entered."""
+
+# pswd="python123"
+# while True :
+#     text=str(input("Enter The Password : "))
+#     if pswd==text :
+#         print("Entered Password Is Correct ")
+#         break 
+#     else:
+#         print("Wrong Password!!! , Try Again")
+
+"""Question 7 (While Loop)
+Check whether a given number is a Prime Number."""
+
+# num=int(input("Enter The Number : ").strip())
+# if num<=1:
+#     print("Invalid Number")
+# else:
+#     i=2
+#     while i<num:
+#         if num%i==0:
+#             print("Number Is Not Prime")
+#             break
+#         i+=1
+#     else:
+#             print("Number Is prime")
+
+"""Question 8 (While)
+Create a Number Guessing Game.
+Rules:
+Secret number = 27
+Keep asking the user to guess until they get it right.
+Print "Too High" or "Too Low" after each wrong guess.
+Count the total number of attempts.
+After the correct guess, print a star pattern where the number of stars equals the number of attempts.
+Example
+Guess: 15
+Too Low
+Guess: 40
+Too High
+Guess: 27
+Correct!
+Attempts: 3"""
+
+secret_number=39
+attempt=0
+while True:
+    guess=int(input("Guess Your Number : "))
+    attempt+=1
+    if guess>secret_number:
+        print("Too High")
+    elif secret_number>guess:
+        print("Too Low")
+    else:
+        print("You Guessed It Correct !!")
+        print(f"You Made {attempt} Attempts")
+        print("Star Pattern:")
+        for j in range(1,attempt+1):
+            print("*"*j)
+        break
