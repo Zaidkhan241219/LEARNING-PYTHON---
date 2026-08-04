668c67aa454b7087729284e163e4c484b28cfd86
ZAID KHAN <zk14062006@gmail.com>
2026-08-05 01:47:28 +0530

    lists done

---
+++ Python_Chp/Chp_09/9.1_Lists/accessing_value.py
@@ -0,0 +1,13 @@
+# File Name :- accessing_value.py
+
+student_1 = ["Rahul" , 19 , 99.5 , True]
+
+print(student_1[0])
+print(student_1[1])
+print(student_1[2])
+print(student_1[3])
+
+numbers = [100 , 10.2 , 3366 , 85]
+
+print(numbers[-2])
+print(numbers[-2:])
---
+++ Python_Chp/Chp_09/9.1_Lists/creating_list.py
@@ -0,0 +1,11 @@
+# File Name :- creating_list.py
+
+numbers = [100 , 10.2 , 3366 , 85]
+strings = ["potato" , "onion" , "tomato"]
+student_1 = ["Rahul" , 19 , 99.5 , True]
+empty_list = []
+
+print(numbers)
+print(strings)
+print(student_1)
+print(empty_list)
