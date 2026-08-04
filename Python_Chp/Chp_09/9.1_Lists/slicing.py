3a67db209ff91f5ee51498fefcf8084b41700a9c
ZAID KHAN <zk14062006@gmail.com>
2026-08-05 01:54:48 +0530

    lists done

---
+++ Python_Chp/Chp_09/9.1_Lists/slicing.py
@@ -0,0 +1,9 @@
+# File Name :- slicing.py
+
+numbers=[100 , 300 , 700 , 10000 , 33.44 , 55.6457 , 99 , 2 , 45]
+
+print(numbers[1:3])
+print(numbers[1:])
+print(numbers[:3])
+print(numbers[::3])
+print(numbers[::-1])
---
+++ Python_Chp/Chp_09/9.1_Lists/updating_values.py
@@ -0,0 +1,9 @@
+# File Name :- updating_values.py
+
+student_1 = ["Rahul" , 19 , 99.5 , True]
+
+student_1[3]=False
+print(student_1)
+
+student_1[1:3]=[22,333.66]
+print(student_1)
