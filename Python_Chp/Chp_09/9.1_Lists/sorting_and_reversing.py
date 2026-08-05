ebb3293e6a2198bdaaf31e3593de878428f145c4
ZAID KHAN <zk14062006@gmail.com>
2026-08-05 13:46:42 +0530

    lists done

--- Python_Chp/Chp_09/9.1_Lists/sorting_and_reversing.py
+++ Python_Chp/Chp_09/9.1_Lists/sorting_and_reversing.py
@@ -4,3 +4,14 @@ numbers=[100 , 300 , 700 , 10000 , 33.44 , 55.6457 , 99 , 2 , 45]
 
 numbers.sort()
 print(numbers)
+
+numbers.sort(reverse=True)
+print(numbers)
+
+numbers.reverse()
+print(numbers)
+
+print(sorted(numbers)) # ye original list ko change nhi krta .sort() aur .reverse() ki tarah 
+
+print(numbers)
+
